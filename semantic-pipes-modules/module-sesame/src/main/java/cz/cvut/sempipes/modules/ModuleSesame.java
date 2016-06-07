f73578e7ee9336af9718f3599028f33f40802ada
Petr Křemen <petr.kremen@fel.cvut.cz>
2016-06-07 18:16:12 +0200

    [NEW] Sesame module initial version

---
+++ semantic-pipes-modules/module-sesame/src/main/java/cz/cvut/sempipes/modules/ModuleSesame.java
@@ -0,0 +1,95 @@
+package cz.cvut.sempipes.modules;
+
+import cz.cvut.sempipes.engine.ExecutionContext;
+import cz.cvut.sempipes.engine.ExecutionContextFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.openrdf.model.Resource;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.http.HTTPRepository;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class ModuleSesame extends AbstractModule {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ApplyConstructModule.class);
+
+    private static final String BASE_IRI = "http://onto.fel.cvut.cz/ontologies/lib/module/";
+    private static final String MODULE_IRI = BASE_IRI + "sesame";
+
+    private static Property getParameter(final String name) {
+        return ResourceFactory.createProperty(MODULE_IRI + "/" + name);
+    }
+
+    static final Property P_SESAME_SERVER_URL = getParameter("p-sesame-server-url");
+    private String sesameServerURL;
+
+    static final Property P_SESAME_REPOSITORY_NAME = getParameter("p-sesame-repository-name");
+    private String sesameRepositoryName;
+
+    static final Property P_SESAME_CONTEXT_IRI = getParameter("p-sesame-context-iri");
+    private String sesameContextIRI;
+
+    static final Property P_IS_REPLACE_CONTEXT_IRI = getParameter("p-is-replace");
+    private boolean isReplaceContext;
+
+    @Override
+    public ExecutionContext execute(ExecutionContext context) {
+        final Repository repository = new HTTPRepository(sesameServerURL, sesameRepositoryName );
+        RepositoryConnection connection = null;
+
+        try {
+            repository.initialize();
+            connection = repository.getConnection();
+
+            final Resource sesameContextIRIResource = (sesameContextIRI == null || sesameContextIRI.isEmpty()) ? null : connection.getValueFactory().createURI(sesameContextIRI);
+
+            connection.begin();
+            if (isReplaceContext) {
+                connection.clear( sesameContextIRIResource );
+            }
+
+            StringWriter w = new StringWriter();
+            context.getDefaultModel().write(w,"N-TRIPLES");
+
+            connection.add(new StringReader(w.getBuffer().toString()), "", RDFFormat.N3, sesameContextIRIResource);
+            connection.commit();
+        } catch (final RepositoryException | RDFParseException | IOException e) {
+            LOG.error(e.getMessage(),e);
+        } finally {
+            try {
+                if (connection != null && connection.isOpen()) {
+                    connection.close();
+                }
+            } catch (RepositoryException e) {
+                LOG.error(e.getMessage(), e);
+            } finally {
+                if (repository.isInitialized()) {
+                    try {
+                        repository.shutDown();
+                    } catch (RepositoryException e) {
+                        LOG.error("During finally: ", e);
+                    }
+                }
+            }
+        }
+
+        return ExecutionContextFactory.createContext(context.getDefaultModel());
+    }
+
+    @Override
+    public void loadConfiguration(org.apache.jena.rdf.model.Resource moduleRes) {
+        sesameServerURL = this.getStringPropertyValue(P_SESAME_SERVER_URL);
+        sesameRepositoryName = this.getStringPropertyValue(P_SESAME_REPOSITORY_NAME);
+        sesameContextIRI = this.getStringPropertyValue(P_SESAME_CONTEXT_IRI);
+        isReplaceContext = this.getPropertyValue(P_IS_REPLACE_CONTEXT_IRI, false);
+    }
+}
---
+++ semantic-pipes-modules/module-sesame/src/test/java/cz/cvut/sempipes/modules/ModuleSesameTest.java
@@ -0,0 +1,32 @@
+package cz.cvut.sempipes.modules;
+
+import cz.cvut.sempipes.engine.ExecutionContext;
+import cz.cvut.sempipes.engine.ExecutionContextFactory;
+import org.apache.jena.rdf.model.*;
+
+public class ModuleSesameTest {
+
+    @org.junit.Test
+    public void testDeployEmpty() throws Exception {
+        final ModuleSesame moduleSesame = new ModuleSesame();
+
+        final Model deployModel = ModelFactory.createDefaultModel();
+        final Property resource = ResourceFactory.createProperty("http://a");
+        deployModel.add(resource, resource, resource);
+
+        final ExecutionContext executionContext = ExecutionContextFactory.createContext(deployModel);
+
+        final Model model = ModelFactory.createDefaultModel();
+        final Resource root = model.createResource();
+        model.add(root,ModuleSesame.P_IS_REPLACE_CONTEXT_IRI, model.createTypedLiteral(true));
+        model.add(root,ModuleSesame.P_SESAME_SERVER_URL, "http://localhost:18080/openrdf-sesame");
+        model.add(root,ModuleSesame.P_SESAME_REPOSITORY_NAME, "test-semantic-pipes");
+        model.add(root,ModuleSesame.P_SESAME_CONTEXT_IRI, "");
+
+        moduleSesame.setResource(root);
+        moduleSesame.loadConfiguration(null);
+
+        // TODO: currently running server is needed;
+        // moduleSesame.execute(executionContext);
+    }
+}
