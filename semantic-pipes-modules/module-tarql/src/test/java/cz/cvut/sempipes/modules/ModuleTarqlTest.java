30937508d02d34eda12d3487f7493ceadc514e8d
Petr Křemen <petr.kremen@fel.cvut.cz>
2016-06-08 21:37:16 +0200

    [FIX] modules poms

---
+++ semantic-pipes-modules/module-tarql/src/test/java/cz/cvut/sempipes/modules/ModuleTarqlTest.java
@@ -0,0 +1,32 @@
+package cz.cvut.sempipes.modules;
+
+import cz.cvut.sempipes.engine.ExecutionContext;
+import cz.cvut.sempipes.engine.ExecutionContextFactory;
+import org.apache.jena.rdf.model.*;
+
+public class ModuleTarqlTest {
+
+    @org.junit.Test
+    public void testDeployEmpty() throws Exception {
+        final ModuleTarql module = new ModuleTarql();
+
+        final Model deployModel = ModelFactory.createDefaultModel();
+        final Property resource = ResourceFactory.createProperty("http://a");
+        deployModel.add(resource, resource, resource);
+
+        final ExecutionContext executionContext = ExecutionContextFactory.createContext(deployModel);
+
+        final Model model = ModelFactory.createDefaultModel();
+        final Resource root = model.createResource();
+        model.add(root, ModuleTarql.P_IS_REPLACE_CONTEXT_IRI, model.createTypedLiteral(true));
+        model.add(root, ModuleTarql.P_SESAME_SERVER_URL, "http://localhost:18080/openrdf-sesame");
+        model.add(root, ModuleTarql.P_SESAME_REPOSITORY_NAME, "test-semantic-pipes");
+        model.add(root, ModuleTarql.P_SESAME_CONTEXT_IRI, "");
+
+//        moduleSesame.setResource(root);
+//        moduleSesame.loadConfiguration(null);
+
+        // TODO: currently running server is needed;
+        module.execute(executionContext);
+    }
+}
