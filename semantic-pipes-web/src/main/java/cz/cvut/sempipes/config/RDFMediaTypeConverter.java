76602ec5cb3bcc934ecee57d2d331c9ea7e125f1
Petr Křemen <petr.kremen@fel.cvut.cz>
2016-06-12 21:09:33 +0200

    [FIX] tests for RDF mimetypes, code cleanup

--- semantic-pipes-web/src/main/java/cz/cvut/sempipes/config/RDFMediaTypeConverter.java
+++ semantic-pipes-web/src/main/java/cz/cvut/sempipes/config/RDFMediaTypeConverter.java
@@ -1,9 +1,14 @@
 package cz.cvut.sempipes.config;
 
 import cz.cvut.sempipes.util.RDFMimeType;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.RDFLanguages;
+import org.slf4j.Logger;
 import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
@@ -13,6 +18,8 @@ import java.io.IOException;
 
 @Component
 public class RDFMediaTypeConverter extends AbstractHttpMessageConverter {
+
+    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(RDFMediaTypeConverter.class);
 
     public RDFMediaTypeConverter() {
         super(
@@ -24,18 +31,35 @@ public class RDFMediaTypeConverter extends AbstractHttpMessageConverter {
         );
     }
 
+    private String getRDFLanguageForContentType( final HttpMessage m, final String defaultValue) {
+        MediaType contentType = m.getHeaders().getContentType();
+        if ( contentType == null ) { contentType = MediaType.parseMediaType(defaultValue); }
+        return RDFLanguages.contentTypeToLang(contentType.toString()).getLabel();
+    }
+
     @Override
     protected Object readInternal(Class aClass, HttpInputMessage httpInputMessage) throws IOException, HttpMessageNotReadableException {
-        return httpInputMessage.getBody();
+        if (  ! aClass.isAssignableFrom( Model.class ) ) {
+            throw new UnsupportedOperationException();
+        }
+        LOG.debug("Reading " + aClass + ", message: " + httpInputMessage.getHeaders());
+        Model inputDataModel = ModelFactory.createDefaultModel();
+        inputDataModel.read(httpInputMessage.getBody(), "", getRDFLanguageForContentType(httpInputMessage, RDFMimeType.N_TRIPLES_STRING));
+        return inputDataModel;
     }
 
     @Override
     protected void writeInternal(Object o, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
-        throw new UnsupportedOperationException();
+        if ( ! ( o instanceof Model ) ) {
+            throw new UnsupportedOperationException();
+        }
+        LOG.debug("Writing " + o + ", message: " + httpOutputMessage.getHeaders());
+        ((Model) o).write(httpOutputMessage.getBody(), getRDFLanguageForContentType(httpOutputMessage, RDFMimeType.LD_JSON_STRING));
     }
 
     @Override
     protected boolean supports(Class aClass) {
-        return true;
+        System.out.println("Supports? "+aClass);
+        return Model.class.isAssignableFrom(aClass);
     }
 }
--- semantic-pipes-web/src/test/java/cz/cvut/sempipes/web/SempipesServiceControllerTest.java
+++ semantic-pipes-web/src/test/java/cz/cvut/sempipes/web/SempipesServiceControllerTest.java
@@ -1,18 +1,16 @@
 package cz.cvut.sempipes.web;
 
 import cz.cvut.sempipes.config.WebAppConfig;
-import cz.cvut.sempipes.constants.SML;
-import cz.cvut.sempipes.engine.ExecutionContext;
-import cz.cvut.sempipes.engine.ExecutionContextFactory;
 import cz.cvut.sempipes.engine.VariablesBinding;
-import cz.cvut.sempipes.modules.ApplyConstructModule;
 import cz.cvut.sempipes.service.SempipesServiceController;
 import cz.cvut.sempipes.util.RDFMimeType;
+import org.apache.jena.ext.com.google.common.collect.Lists;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFLanguages;
 import org.apache.jena.vocabulary.RDFS;
 import org.junit.Assert;
 import org.junit.Before;
@@ -43,6 +41,11 @@ public class SempipesServiceControllerTest {
 
     private MockMvc mockMvc;
 
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
     @Test
     public void testRunNoModule() throws Exception {
         final RequestBuilder rb = get("/module");
@@ -52,111 +55,57 @@ public class SempipesServiceControllerTest {
     @Test
     public void testRunNonExistingModule() throws Exception {
         final RequestBuilder rb = get("/module").
-                param(SempipesServiceController.P_ID,"http://onto.fel.cvut.cz/ontologies/eccairs/aviation-3.4.0.2/sempipes");
+                param(SempipesServiceController.P_ID,"http://example.org/sempipes/test-no-module");
         mockMvc.perform(rb).andExpect(status().is4xxClientError());
     }
 
     @Test
-    public void testRunExistingModule() throws Exception {
-        // TODO identity transformer resolution
-        final RequestBuilder rb = post("/module").
+    public void testRunInvalidConfigurationModule() throws Exception {
+        final RequestBuilder rb = get("/module").
                 param(SempipesServiceController.P_ID,"http://onto.fel.cvut.cz/ontologies/sempipes/identity-transformer").
-                param(SempipesServiceController.P_CONFIG_URL,getClass().getResource("/module-identity/config.ttl").toString()).
+                param(SempipesServiceController.P_CONFIG_URL,"http://example.org/sempipes/test-no-module/no-configuration");
+        mockMvc.perform(rb).andExpect(status().is4xxClientError());
+    }
+
+    private MockHttpServletRequestBuilder createDefaultIdentityModuleBuilder() throws Exception {
+        // TODO identity transformer resolution
+        return post("/module").
+                param(SempipesServiceController.P_ID,"http://onto.fel.cvut.cz/ontologies/sempipes/identity-transformer").
+                param(SempipesServiceController.P_CONFIG_URL,getClass().getResource("/module-identity/config.ttl").toURI().toURL().toString()).
                 param("paramString","haha").
                 param("paramInt","7").
                 param("paramIRI","http://test.me").
-                accept(RDFMimeType.LD_JSON_STRING).
                 contentType(RDFMimeType.N_TRIPLES_STRING).
-                content("<http://a> <http://a> <http://a> .");
-        MvcResult result = mockMvc.perform(rb)
-//                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk()).andReturn();
+                content("<http://a/b> <http://a/b> <http://a/b> .");
+    }
+
+    @Test
+    public void testRunExistingModule() throws Exception {
+        MvcResult result = mockMvc.perform(createDefaultIdentityModuleBuilder().
+                accept(RDFMimeType.LD_JSON_STRING)
+        ).andExpect(status().isOk()).andReturn();
         System.out.println("Resulting JSON: " + result.getResponse().getContentAsString());
     }
 
-//    @Test
-//    public void testAcceptJSONLD() throws Exception {
-//        testMimeType(RDFMimeType.LD_JSON_STRING, true);
-//    }
-//    @Test
-//    public void testAcceptRDFXML() throws Exception {
-//        testMimeType(RDFMimeType.RDF_XML_STRING, true);
-//    }
+    @Test
+    public void testAcceptRDFMimeTypes() throws Exception {
+        testMimeType(RDFMimeType.N_TRIPLES_STRING, true);
+        testMimeType(RDFMimeType.TURTLE_STRING, true);
+        testMimeType(RDFMimeType.LD_JSON_STRING, true);
+        testMimeType(RDFMimeType.RDF_XML_STRING, true);
+    }
 
     private void testMimeType( final String mimeType, boolean pass ) throws Exception {
-        final RequestBuilder rb = post("/module").
-                param(SempipesServiceController.P_ID,"http://onto.fel.cvut.cz/ontologies/sempipes/identity-transformer").
-                accept(mimeType);
-        mockMvc.perform(rb)
-                .andExpect(pass ? status().isOk() : status().is(415)).andReturn();
-    }
-
-    @Test
-    public void testRunApplyConstructNotReplace() throws Exception {
-        File outputBindingFile = File.createTempFile("sempipes-test-output-binding","ttl");
-        final StringWriter w = new StringWriter();
-        createSimpleModel().write(w,"TURTLE");
-
-        final MockHttpServletRequestBuilder rb = post("/module").
-                param(SempipesServiceController.P_ID, "http://onto.fel.cvut.cz/ontologies/test/apply-construct#CreateSampleTriples").
-                param(SempipesServiceController.P_CONFIG_URL, getClass().getResource("/module-apply-construct/config.ttl").toString()).
-                param(SempipesServiceController.P_OUTPUT_BINDING_URL,outputBindingFile.toURI().toURL().toString().toString()).
-                accept(RDFMimeType.LD_JSON_STRING).
-                contentType(RDFMimeType.TURTLE_STRING).
-                content(w.getBuffer().toString());
-
-        MvcResult result = mockMvc.perform(rb)
-//                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk()).andReturn();
-        System.out.println("=== No replace parameter");
-        System.out.println("Resulting JSON: " + result.getResponse().getContentAsString());
-        VariablesBinding outputBinding = new VariablesBinding();
-        outputBinding.load(new FileInputStream(outputBindingFile), "TURTLE");
-        System.out.println("Output binding:" + outputBinding);
-
-        final StringReader jsonLDResult = new StringReader(result.getResponse().getContentAsString());
-        final Model mOutput = ModelFactory.createDefaultModel();
-        RDFDataMgr.read(mOutput,jsonLDResult,"", Lang.JSONLD);
-        Assert.assertEquals(mOutput.listStatements().toList().size(), 2);
-    }
-
-    @Test
-    public void testRunApplyConstructQueryWithVariable() throws Exception {
-        final StringWriter inputData = new StringWriter();
-        createSimpleModel().write(inputData,"TURTLE");
-
-        File inputBindingFile = File.createTempFile("sempipes-test-input-binding","ttl");
-        VariablesBinding inputVariablesBinding = new VariablesBinding(
-                "sampleServiceUri",
-                ResourceFactory.createResource("http://martin.inbas.cz/openrdf-sesame/repositories/form-generator?default-graph-uri=http://www.inbas.cz/ontologies/reporting-tool/formGen-977414103")
-        );
-        inputVariablesBinding.save(new FileOutputStream(inputBindingFile),"TURTLE");
-
-        File outputBindingFile = File.createTempFile("sempipes-test-output-binding","ttl");
-
-        final MockHttpServletRequestBuilder rb = post("/module").
-                param(SempipesServiceController.P_ID, "http://onto.fel.cvut.cz/ontologies/test/apply-construct#CreateSampleTriples").
-                param(SempipesServiceController.P_CONFIG_URL, getClass().getResource("/module-apply-construct/remote-query.ttl").toString()).
-                param(SempipesServiceController.P_OUTPUT_BINDING_URL,outputBindingFile.toURI().toURL().toString().toString()).
-                param(SempipesServiceController.P_INPUT_BINDING_URL,inputBindingFile.toURI().toURL().toString().toString()).
-                accept(RDFMimeType.LD_JSON_STRING).
-                contentType(RDFMimeType.TURTLE_STRING).
-                content(inputData.getBuffer().toString());
-
-        MvcResult result = mockMvc.perform(rb)
-//                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk()).andReturn();
-
-        System.out.println("=== No replace parameter");
-        System.out.println("Resulting JSON: " + result.getResponse().getContentAsString());
-        VariablesBinding outputBinding = new VariablesBinding();
-        outputBinding.load(new FileInputStream(outputBindingFile), "TURTLE");
-        System.out.println("Output binding:" + outputBinding);
-
-        final StringReader jsonLDResult = new StringReader(result.getResponse().getContentAsString());
-        final Model mOutput = ModelFactory.createDefaultModel();
-        RDFDataMgr.read(mOutput,jsonLDResult,"", Lang.JSONLD);
-        Assert.assertEquals(mOutput.listStatements().toList().size(), 54);
+        MvcResult result = mockMvc.perform(createDefaultIdentityModuleBuilder().
+                accept(mimeType)
+        ).andExpect(pass ? status().isOk() : status().is(415)).andReturn();
+        System.out.println("Result: " + result.getResponse().getContentAsString());
+        final Model m = ModelFactory.createDefaultModel();
+        try {
+            m.read(new ByteArrayInputStream(result.getResponse().getContentAsByteArray()), "", RDFLanguages.contentTypeToLang(mimeType).getName());
+        } catch(Exception e) {
+            Assert.fail("Could not parse the result back. Reason: " + e.getMessage());
+        }
     }
 
     private Model createSimpleModel() {
@@ -169,8 +118,75 @@ public class SempipesServiceControllerTest {
         return model;
     }
 
-    @Before
-    public void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    private void testModule(
+            String id,
+            String resourceConfig,
+            Model inputModel,
+            VariablesBinding inputVariablesBinding,
+            VariablesBinding expectedOutputVariablesBinding,
+            int expectedNumberOfStatements
+
+    ) throws Exception {
+        if ( inputVariablesBinding == null ) {
+            inputVariablesBinding = new VariablesBinding();
+        }
+        File inputBindingFile = File.createTempFile("sempipes-test-input-binding","ttl");
+        inputVariablesBinding.save(new FileOutputStream(inputBindingFile), "TURTLE");
+
+        if ( expectedOutputVariablesBinding == null ) {
+            expectedOutputVariablesBinding = new VariablesBinding();
+        }
+        File outputBindingFile = File.createTempFile("sempipes-test-output-binding","ttl");
+        final StringWriter w = new StringWriter();
+        inputModel.write(w,"TURTLE");
+
+        final MockHttpServletRequestBuilder rb = post("/module").
+                param(SempipesServiceController.P_ID, id).
+                param(SempipesServiceController.P_CONFIG_URL, getClass().getResource(resourceConfig).toString()).
+                param(SempipesServiceController.P_INPUT_BINDING_URL,inputBindingFile.toURI().toURL().toString()).
+                param(SempipesServiceController.P_OUTPUT_BINDING_URL,outputBindingFile.toURI().toURL().toString()).
+                accept(RDFMimeType.LD_JSON_STRING).
+                contentType(RDFMimeType.TURTLE_STRING).
+                content(w.getBuffer().toString());
+
+        MvcResult result = mockMvc.perform(rb)
+//                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk()).andReturn();
+        System.out.println("Result: " + result.getResponse().getContentAsString());
+        VariablesBinding outputBinding = new VariablesBinding();
+        outputBinding.load(new FileInputStream(outputBindingFile), "TURTLE");
+        Assert.assertEquals(Lists.newArrayList(outputBinding.getVarNames()), Lists.newArrayList(expectedOutputVariablesBinding.getVarNames()));
+
+        final StringReader res = new StringReader(result.getResponse().getContentAsString());
+        final Model mOutput = ModelFactory.createDefaultModel();
+        RDFDataMgr.read(mOutput,res,"", Lang.JSONLD);
+        Assert.assertEquals(mOutput.listStatements().toList().size(), expectedNumberOfStatements);
     }
-}
+
+    @Test
+    public void testRunApplyConstructNotReplace() throws Exception {
+        testModule(
+                "http://onto.fel.cvut.cz/ontologies/test/apply-construct#CreateSampleTriples",
+                "/module-apply-construct/config.ttl",
+                createSimpleModel(),
+                null,
+                null,
+                2);
+    }
+
+    @Test
+    public void testRunApplyConstructQueryWithVariable() throws Exception {
+        VariablesBinding inputVariablesBinding = new VariablesBinding(
+                "sampleServiceUri",
+                ResourceFactory.createResource("http://martin.inbas.cz/openrdf-sesame/repositories/form-generator?default-graph-uri=http://www.inbas.cz/ontologies/reporting-tool/formGen-977414103")
+        );
+
+        testModule(
+                "http://onto.fel.cvut.cz/ontologies/test/apply-construct#CreateSampleTriples",
+                "/module-apply-construct/remote-query.ttl",
+                createSimpleModel(),
+                inputVariablesBinding,
+                null,
+                54);
+    }
+ }
