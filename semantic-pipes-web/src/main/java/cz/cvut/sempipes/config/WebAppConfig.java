336052e15628a80894a6ecb69090d01f9778c208
Miroslav <blcham@gmail.com>
2016-06-12 23:56:41 +0200

    [fix] accept header

--- semantic-pipes-web/src/main/java/cz/cvut/sempipes/config/WebAppConfig.java
+++ semantic-pipes-web/src/main/java/cz/cvut/sempipes/config/WebAppConfig.java
@@ -1,11 +1,14 @@
 package cz.cvut.sempipes.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,6 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import java.nio.charset.Charset;
 import java.util.List;
 
 @Configuration
@@ -49,8 +53,18 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        final RDFMediaTypeConverter RDFConverter = new RDFMediaTypeConverter();
-        converters.add(RDFConverter);
+
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        converter.setObjectMapper(objectMapper);
+        converters.add(converter);
+        final StringHttpMessageConverter stringConverter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        converters.add(stringConverter);
+
+//        final RDFMediaTypeConverter RDFConverter = new RDFMediaTypeConverter();
+//        converters.add(RDFConverter);
         super.configureMessageConverters(converters);
     }
 }
