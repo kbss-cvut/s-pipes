616e69da75ff82f9e558e1a0bca42fb0ef5c550a
Miroslav <blcham@gmail.com>
2016-10-10 14:42:18 +0200

    [new] SUTime module template

---
+++ semantic-pipes-modules/module-nlp/src/main/java/cz/cvut/sempipes/modules/ModuleSUTime.java
@@ -0,0 +1,23 @@
+package cz.cvut.sempipes.modules;
+
+import cz.cvut.sempipes.engine.ExecutionContext;
+
+/**
+ * Created by Miroslav Blasko on 10.10.16.
+ */
+public class ModuleSUTime extends AbstractModule {
+    @Override
+    public String getTypeURI() {
+        return null;
+    }
+
+    @Override
+    public void loadConfiguration() {
+
+    }
+
+    @Override
+    ExecutionContext executeSelf() {
+        return null;
+    }
+}
---
+++ semantic-pipes-modules/module-nlp/src/test/java/cz/cvut/sempipes/modules/ModuleSUTimeTest.java
@@ -0,0 +1,27 @@
+package cz.cvut.sempipes.modules;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Miroslav Blasko on 10.10.16.
+ */
+public class ModuleSUTimeTest {
+    @Test
+    public void loadConfiguration() throws Exception {
+
+    }
+
+    @Test
+    public void executeSelf() throws Exception {
+
+    }
+
+
+    @Test
+    public void simpleTest() throws Exception {
+
+    }
+
+}
