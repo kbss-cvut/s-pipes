4942fe758da35d64b60929efcbee889ac2a21318
Petr Kremen <kremep1@fel.cvut.cz>
2016-10-21 00:15:15 +0200

    [FIX] missing vocabulary

---
+++ semantic-pipes-modules/module-dataset-discovery/src/main/java/cz/cvut/sempipes/modules/datasetdiscovery/model/generated/Vocabulary.java
@@ -0,0 +1,69 @@
+
+package cz.cvut.sempipes.modules.datasetdiscovery.model.generated;
+
+
+public class Vocabulary {
+
+    public final static String ONTOLOGY_IRI_dataset_descriptor = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor";
+    public final static String ONTOLOGY_IRI_dataset_descriptor_generated = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor-generated";
+    public final static String s_p_has_target = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/has-target";
+    public final static String s_p_INV_dot_describes = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.describes";
+    public final static String s_c_Thing = "http://www.w3.org/2002/07/owl#Thing";
+    public final static String s_p_has_item = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/has-item";
+    public final static String s_c_dataset_snapshot = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/dataset-snapshot";
+    public final static String s_c_sparql_endpoint = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/sparql-endpoint";
+    public final static String s_c_target_dataset = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/target-dataset";
+    public final static String s_p_INV_dot_componentOf = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.componentOf";
+    public final static String s_c_dataset_selector = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/dataset-selector";
+    public final static String s_c_data_quality_assessment_purpose = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/data-quality-assessment-purpose";
+    public final static String s_c_temporal_descriptor = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/temporal-descriptor";
+    public final static String s_p_INV_dot_has_sub_dataset_instance = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.has-sub-dataset-instance";
+    public final static String s_p_has_sub_dataset_instance = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/has-sub-dataset-instance";
+    public final static String s_c_dataset = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/dataset";
+    public final static String s_p_has_source = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/has-source";
+    public final static String s_c_sparql_construct = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/sparql-construct";
+    public final static String s_c_dataset_aggregator = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/dataset-aggregator";
+    public final static String s_p_INV_dot_offers = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.offers";
+    public final static String s_p_has_dataset = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/has-dataset";
+    public final static String s_p_memberOf = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/memberOf";
+    public final static String s_p_describes = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/describes";
+    public final static String s_c_source_dataset = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/source-dataset";
+    public final static String s_p_INV_dot_subCollectionOf = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.subCollectionOf";
+    public final static String s_c_dataset_source = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/dataset-source";
+    public final static String s_c_single_item_selector = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/single-item-selector";
+    public final static String s_p_INV_dot_has_member = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.has-member";
+    public final static String s_p_GS = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/GS";
+    public final static String s_p_was_obtained_from = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/was-obtained-from";
+    public final static String s_p_INV_dot_was_obtained_from = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.was-obtained-from";
+    public final static String s_p_label = "http://www.w3.org/2000/01/rdf-schema#label";
+    public final static String s_p_INV_dot_was_produced_by = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.was-produced-by";
+    public final static String s_p_is_purpose_of = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/is-purpose-of";
+    public final static String s_p_is_part_of = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/is-part-of";
+    public final static String s_c_property_partition = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/property-partition";
+    public final static String s_c_spatial_descriptor = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/spatial-descriptor";
+    public final static String s_p_INV_dot_has_dataset = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.has-dataset";
+    public final static String s_p_INV_dot_is_purpose_of = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.is-purpose-of";
+    public final static String s_p_has_member = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/has-member";
+    public final static String s_c_transformation = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/transformation";
+    public final static String s_p_componentOf = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/componentOf";
+    public final static String s_p_INV_dot_has_source = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.has-source";
+    public final static String s_p_comment = "http://www.w3.org/2000/01/rdf-schema#comment";
+    public final static String s_c_class_partition = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/class-partition";
+    public final static String s_p_INV_dot_memberOf = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.memberOf";
+    public final static String s_c_vocabulary_instance = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/vocabulary-instance";
+    public final static String s_p_INV_dot_has_item = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.has-item";
+    public final static String s_c_entity_partition = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/entity-partition";
+    public final static String s_c_descriptor_purpose = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/descriptor-purpose";
+    public final static String s_p_subCollectionOf = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/subCollectionOf";
+    public final static String s_p_offers = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/offers";
+    public final static String s_p_constitutes = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/constitutes";
+    public final static String s_p_INV_dot_is_part_of = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.is-part-of";
+    public final static String s_c_shared_ontology_descriptor = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/shared-ontology-descriptor";
+    public final static String s_c_data = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/data";
+    public final static String s_p_INV_dot_constitutes = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.constitutes";
+    public final static String s_p_was_produced_by = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/was-produced-by";
+    public final static String s_p_INV_dot_has_target = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/INV.has-target";
+    public final static String s_c_data_item = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/data-item";
+    public final static String s_c_dataset_descriptor = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/dataset-descriptor";
+
+}
