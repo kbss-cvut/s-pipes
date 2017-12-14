3cb8b9d423d1e0fc98a86a105c9196171259a941
Yan Doroshenko <yan1994doroshenko@gmail.com>
2017-12-14 16:47:52 +0100

    [new] Implement the question ordering test
    (cherry picked from commit ad3142b)

--- semantic-pipes-forms/src/test/java/cz/cvut/sempipes/transform/TransformerImplTest.java
+++ semantic-pipes-forms/src/test/java/cz/cvut/sempipes/transform/TransformerImplTest.java
@@ -1,16 +1,21 @@
 package cz.cvut.sempipes.transform;
 
+import cz.cvut.sempipes.VocabularyJena;
 import cz.cvut.sempipes.constants.SML;
 import cz.cvut.sempipes.model.qam.Question;
-import java.io.InputStream;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.util.FileUtils;
 import org.apache.jena.vocabulary.RDFS;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import org.junit.Test;
 
 public class TransformerImplTest {
 
@@ -28,15 +33,28 @@ public class TransformerImplTest {
     @Test
     public void script2FormCreatesIdSubQuestion() {
         assertTrue(generateRootQuestion().getSubQuestions().stream()
-            .anyMatch(s -> RDFS.Resource.getURI().equals(s.getOrigin().toString())));
+                .anyMatch(s -> RDFS.Resource.getURI().equals(s.getOrigin().toString())));
     }
 
     @Test
     public void script2FormOrdersQuestionsCorrectly() {
         Question root = generateRootQuestion();
-        root.getSubQuestions().stream().forEach((q) -> {
+        Question idQuestion = root.getSubQuestions().stream()
+                .filter(s -> RDFS.Resource.getURI().equals(s.getOrigin().toString()))
+                .collect(Collectors.toList()).get(0);
+        Optional<Question> labelQuestion = root.getSubQuestions().stream()
+                .filter(s -> s.getOrigin().toString().startsWith(VocabularyJena.s_c_question_origin.toString()))
+                .findFirst();
 
-        });
+        assertTrue(root.getSubQuestions().stream().
+                filter((question -> !idQuestion.equals(question)))
+                .allMatch((question -> isPrecedingQuestion(idQuestion, question))));
+        assertTrue(!labelQuestion.isPresent() ||
+                root.getSubQuestions().stream()
+                        .filter(question -> !idQuestion.equals(question))
+                        .filter(question -> !labelQuestion.get().equals(question))
+                        .allMatch((question -> isPrecedingQuestion(labelQuestion.get(), question))) &&
+                        isPrecedingQuestion(idQuestion, labelQuestion.get()));
     }
 
     private Question generateRootQuestion() {
@@ -45,10 +63,21 @@ public class TransformerImplTest {
         Model sampleScript = ModelFactory.createDefaultModel().read(sampleScriptIS, null, FileUtils.langTurtle);
 
         Resource module = sampleScript.listSubjects().filterKeep(s ->
-            s.getURI() != null && s.getURI().endsWith("bind-person")
+                s.getURI() != null && s.getURI().endsWith("bind-person")
         ).next();
         Resource moduleType = sampleScript.getResource(SML.BindWithConstant.toString());
 
         return new TransformerImpl().script2Form(sampleScript, module, moduleType);
+    }
+
+    private boolean isPrecedingQuestion(Question preceding, Question following) {
+        if (following.getPrecedingQuestions().contains(preceding)) {
+            return true;
+        }
+        else if (following.getPrecedingQuestions() != null && !following.getPrecedingQuestions().isEmpty()) {
+            return following.getPrecedingQuestions().parallelStream()
+                    .anyMatch((q) -> isPrecedingQuestion(preceding, q));
+        }
+        return false;
     }
 }
