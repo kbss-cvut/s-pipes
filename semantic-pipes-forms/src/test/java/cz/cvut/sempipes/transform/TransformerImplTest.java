7f901e7bbcb31056444e3a0ede0630e5faf6b1e9
Yan Doroshenko <yan.doroshenko@fel.cvut.cz>
2018-01-31 17:11:35 +0100

    [fix] Fix tests (taking layoutClass into consideration)

--- semantic-pipes-forms/src/test/java/cz/cvut/sempipes/transform/TransformerImplTest.java
+++ semantic-pipes-forms/src/test/java/cz/cvut/sempipes/transform/TransformerImplTest.java
@@ -10,6 +10,8 @@ import org.apache.jena.vocabulary.RDFS;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -21,36 +23,53 @@ public class TransformerImplTest {
 
     @Test
     public void script2FormCreatesSubQuestions() {
-        assertEquals(4, generateRootQuestion().getSubQuestions().size());
+        assertEquals(1, generateRootQuestion().getSubQuestions().size());
+        assertEquals(4, generateRootQuestion().getSubQuestions().stream()
+                .flatMap((q) -> q.getSubQuestions().stream())
+                .collect(Collectors.toList())
+                .size());
     }
 
     @Test
-    public void script2FormCreatesSetsRootQuestionLabel() {
+    public void script2FormCreatesCorrectLayoutClasses() {
+        assertTrue(generateRootQuestion().getLayoutClass().contains("form"));
+        assertTrue(generateRootQuestion().getSubQuestions().stream()
+                .allMatch((s) -> s.getLayoutClass().containsAll(new HashSet<String>() {{
+                    add("section");
+                    add("wizard-step");
+                }})));
+    }
+
+    @Test
+    public void script2FormCreatesCorrectLabels() {
         assertEquals("Module of type Bind with constant", generateRootQuestion().getLabel());
+        assertTrue(generateRootQuestion().getSubQuestions().stream()
+                .allMatch((q) -> q.getLabel().equals("Module configuration")));
     }
 
     @Test
     public void script2FormCreatesIdSubQuestion() {
         assertTrue(generateRootQuestion().getSubQuestions().stream()
+                .flatMap((q) -> q.getSubQuestions().stream())
                 .anyMatch(s -> RDFS.Resource.getURI().equals(s.getOrigin().toString())));
     }
 
     @Test
     public void script2FormOrdersQuestionsCorrectly() {
-        Question root = generateRootQuestion();
-        Question idQuestion = root.getSubQuestions().stream()
+        Question wizardStep = new LinkedList<>(generateRootQuestion().getSubQuestions()).get(0);
+        Question idQuestion = wizardStep.getSubQuestions().stream()
                 .filter(s -> RDFS.Resource.getURI().equals(s.getOrigin().toString()))
                 .collect(Collectors.toList()).get(0);
 
-        Optional<Question> labelQuestion = root.getSubQuestions().stream()
+        Optional<Question> labelQuestion = wizardStep.getSubQuestions().stream()
                 .filter(s -> s.getOrigin().toString().equals(RDFS.label.toString()))
                 .findFirst();
 
-        assertTrue(root.getSubQuestions().stream().
+        assertTrue(wizardStep.getSubQuestions().stream().
                 filter((question -> !idQuestion.equals(question)))
                 .allMatch((question -> isPrecedingQuestion(idQuestion, question))));
         assertTrue(!labelQuestion.isPresent() ||
-                root.getSubQuestions().stream()
+                wizardStep.getSubQuestions().stream()
                         .filter(question -> !idQuestion.equals(question))
                         .filter(question -> !labelQuestion.get().equals(question))
                         .allMatch((question -> isPrecedingQuestion(labelQuestion.get(), question))) &&
@@ -60,7 +79,7 @@ public class TransformerImplTest {
     @Test
     public void script2FormAllQuestionsHaveAnswer() {
         Question root = generateRootQuestion();
-        assertTrue(root.getSubQuestions().stream().allMatch((q) -> q.getAnswers() != null && !q.getAnswers().isEmpty()));
+        assertTrue(root.getSubQuestions().stream().flatMap((q) -> q.getSubQuestions().stream()).allMatch((q) -> q.getAnswers() != null && !q.getAnswers().isEmpty()));
     }
 
     private Question generateRootQuestion() {
