01bf53cc5b1c20000a175934dfab10c2bb476f72
Miroslav Blasko <blcham@gmail.com>
2018-04-26 17:51:53 +0200

    [upd] refactoring and documentation of progress listener api

--- semantic-pipes-core/src/main/java/cz/cvut/sempipes/constants/SPIPES.java
+++ semantic-pipes-core/src/main/java/cz/cvut/sempipes/constants/SPIPES.java
@@ -25,10 +25,14 @@ public class SPIPES {
     public static final Property has_classname = property("has-classname");
 
     public static final Property has_module_execution_start_date = property("has-module-execution-start-date");
+    public static final Property has_module_execution_start_date_unix = property("has-module-execution-start-date-unix");
     public static final Property has_module_execution_finish_date = property("has-module-execution-finish-date");
+    public static final Property has_module_execution_finish_date_unix = property("has-module-execution-finish-date-unix");
     public static final Property has_module_execution_duration = property("has-module-execution-duration");
     public static final Property has_pipeline_execution_start_date = property("has-pipeline-execution-start-date");
+    public static final Property has_pipeline_execution_start_date_unix = property("has-pipeline-execution-start-date-unix");
     public static final Property has_pipeline_execution_finish_date = property("has-pipeline-execution-finish-date");
+    public static final Property has_pipeline_execution_finish_date_unix = property("has-pipeline-execution-finish-date-unix");
     public static final Property has_pipeline_execution_duration = property("has-pipeline-execution-duration");
 
     public static final Property has_module_type = property("has-module-type");
