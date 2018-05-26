3aee6cc163ee41b8268f1e4a5d22baf9f6e49123
Miroslav Blasko <blcham@gmail.com>
2018-05-27 00:15:39 +0200

    [new] variable binding restrictTo operation

--- semantic-pipes-core/src/test/java/cz/cvut/sempipes/engine/VariablesBindingTest.java
+++ semantic-pipes-core/src/test/java/cz/cvut/sempipes/engine/VariablesBindingTest.java
@@ -10,6 +10,9 @@ import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+import junit.framework.TestCase;
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.apache.jena.rdf.model.ResourceFactory;
 import static org.hamcrest.CoreMatchers.containsString;
 import org.junit.Assert;
@@ -20,6 +23,21 @@ import org.junit.rules.ExpectedException;
 public class VariablesBindingTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void restrictToReturnsOnlyListedVariables() {
+        final VariablesBinding vb = new VariablesBinding();
+        vb.add("var1", ResourceFactory.createStringLiteral("value1"));
+        vb.add("var2", ResourceFactory.createStringLiteral("value2"));
+        vb.add("var3", ResourceFactory.createStringLiteral("value3"));
+        vb.add("var4", ResourceFactory.createStringLiteral("value4"));
+        VariablesBinding newVB = vb.restrictTo("var2", "var4");
+        assertEquals(getSize(newVB), 2);
+        assertNotNull(newVB.asQuerySolution().get("var2"));
+        assertNotNull(newVB.asQuerySolution().get("var4"));
+        assertEquals(newVB.asQuerySolution().get("var2").toString(), "value2");
+        assertEquals(newVB.asQuerySolution().get("var4").toString(), "value4");
+    }
 
     @Test
     public void loadMultipleBindings() throws Exception {
@@ -75,4 +93,13 @@ public class VariablesBindingTest {
         Spliterator<T> spliterator = Spliterators.spliteratorUnknownSize(iterator, characteristics);
         return StreamSupport.stream(spliterator, false);
     }
+
+    private int getSize(VariablesBinding newVB) {
+        final Integer[] countRef = {0};
+        newVB.getVarNames().forEachRemaining(
+            n -> { countRef[0]++; }
+        );
+        return countRef[0];
+    }
+
 }
