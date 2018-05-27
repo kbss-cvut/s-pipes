9c3e91fab3325ca2a39270c4db1e34513046018b
Miroslav Blasko <blcham@gmail.com>
2018-05-27 04:01:12 +0200

    [new] variable binding utils

---
+++ semantic-pipes-modules/module-sparql-endpoint/src/main/java/cz/cvut/sempipes/util/VariableBindingUtils.java
@@ -0,0 +1,19 @@
+package cz.cvut.sempipes.util;
+
+import cz.cvut.sempipes.engine.VariablesBinding;
+import java.util.Arrays;
+
+public class VariableBindingUtils {
+
+    public static VariablesBinding restrict(VariablesBinding variablesBinding, String... varNames) {
+        VariablesBinding newVB = variablesBinding.restrictTo(varNames);
+        Arrays.stream(varNames).forEach(
+            vn ->  {if (newVB.getNode(vn) == null) {
+                throw new IllegalStateException(
+                    String.format("Variable binding %s does not contain variable with name %s.\n", variablesBinding, vn)
+                );
+            } }
+        );
+        return newVB;
+    }
+}
