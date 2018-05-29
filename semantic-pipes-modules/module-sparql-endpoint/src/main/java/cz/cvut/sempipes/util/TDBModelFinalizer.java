29cc23129661e492f167ced1d8be8be004e2c873
Miroslav Blasko <blcham@gmail.com>
2018-05-29 14:58:06 +0200

    [new] automatic cleanup of TDB repositories

---
+++ semantic-pipes-modules/module-sparql-endpoint/src/main/java/cz/cvut/sempipes/util/TDBModelFinalizer.java
@@ -0,0 +1,31 @@
+package cz.cvut.sempipes.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+import org.apache.commons.io.FileUtils;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.tdb.store.GraphTDB;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TDBModelFinalizer extends PhantomReference<Model> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TDBModelFinalizer.class);
+    private final String datasetLocation;
+
+    public TDBModelFinalizer(Model referent, ReferenceQueue<? super Model> q) {
+        super(referent, q);
+        this.datasetLocation = ((GraphTDB) referent.getGraph()).getDSG().getLocation().getDirectoryPath();
+    }
+
+    public void finalizeResources() {
+        LOG.debug("Removing temporary TDB dataset at directory {}.", datasetLocation);
+        try {
+            FileUtils.deleteDirectory(new File(datasetLocation));
+        } catch (IOException e) {
+            LOG.error("Could not remove directory at {}, reason: {}.", datasetLocation, e);
+        }
+    }
+}
