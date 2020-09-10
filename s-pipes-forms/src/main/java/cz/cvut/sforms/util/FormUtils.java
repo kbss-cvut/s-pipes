1033e125e00cc2c264b47df722c281bdd268dbc9
Miroslav Blasko <blcham@gmail.com>
2020-09-10 22:45:56 +0200

    [Upd] Documentation of formUtils

--- s-pipes-forms/src/main/java/cz/cvut/sforms/util/FormUtils.java
+++ s-pipes-forms/src/main/java/cz/cvut/sforms/util/FormUtils.java
@@ -1,18 +1,26 @@
 package cz.cvut.sforms.util;
 
 import cz.cvut.sforms.model.Question;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.jetbrains.annotations.NotNull;
 
 public class FormUtils {
 
     public static String SFORMS_MODEL_PACKAGE_NAME = Question.class.getPackage().getName();
 
-    public static Set<Question> flatten(Set<Question> questions) {
-        return questions.stream().flatMap((q) -> flatten(q).stream()).collect(Collectors.toSet());
+    /**
+     * Returns all questions within a tree rooted by provided <code>rootQuestions<code/>.
+     * The <code>rootQuestions</code> question is included as well.
+     *
+     * @param rootQuestions questions representing root of the question tree.
+     * @return Set of question.
+     */
+    public static Set<Question> flatten(Set<Question> rootQuestions) {
+        return rootQuestions.stream().flatMap((q) -> flatten(q).stream()).collect(Collectors.toSet());
     }
 
     /**
