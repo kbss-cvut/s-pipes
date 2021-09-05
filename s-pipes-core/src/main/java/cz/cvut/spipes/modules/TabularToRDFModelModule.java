28215bc88f16abf38db9d2aebbf98b63dbd7df8e
Zdenek <kotrlzde@fel.cvut.cz>
2021-09-06 00:23:40 +0200

    Preparing for implementation

---
+++ s-pipes-core/src/main/java/cz/cvut/spipes/modules/TabularToRDFModelModule.java
@@ -0,0 +1,21 @@
+package cz.cvut.spipes.modules;
+
+import cz.cvut.spipes.engine.ExecutionContext;
+
+public class TabularToRDFModelModule extends AbstractModule {
+
+    @Override
+    ExecutionContext executeSelf() {
+        return null;
+    }
+
+    @Override
+    public String getTypeURI() {
+        return null;
+    }
+
+    @Override
+    public void loadConfiguration() {
+
+    }
+}
