df34d9725df9bc753b1f7a5df6abc412a03eeeb9
Tsimafei Raro <tsimafei.raro@visap.cz>
2022-07-11 16:29:56 +0200

    Remove old testing code

--- s-pipes-modules-utils/s-pipes-module-archetype/src/main/resources/archetype-resources/src/main/java/__moduleNameCamelCase__Module.java
+++ s-pipes-modules-utils/s-pipes-module-archetype/src/main/resources/archetype-resources/src/main/java/__moduleNameCamelCase__Module.java
@@ -14,30 +14,6 @@ public class $className extends AnnotatedAbstractModule {
     private static final String TYPE_URI = KBSS_MODULE.uri + "$artifactId";
     private static final String TYPE_PREFIX = TYPE_URI + "/";
 
-// todo remove
-//    @Parameter(urlPrefix = TYPE_PREFIX, name = "endpoint-url")
-//    private String endpointUrl;
-//
-//    @Parameter(urlPrefix = TYPE_PREFIX, name = "output-resource-variable")
-//    private String outputResourceVariable;
-//
-//    public String getEndpointUrl() {
-//        return endpointUrl;
-//    }
-//
-//    public void setEndpointUrl(String endpointUrl) {
-//        this.endpointUrl = endpointUrl;
-//    }
-//
-//    public String getOutputResourceVariable() {
-//        return outputResourceVariable;
-//    }
-//
-//    public void setOutputResourceVariable(String outputResourceVariable) {
-//        this.outputResourceVariable = outputResourceVariable;
-//    }
-
-
     @Override
     protected ExecutionContext executeSelf() {
         return executionContext;
