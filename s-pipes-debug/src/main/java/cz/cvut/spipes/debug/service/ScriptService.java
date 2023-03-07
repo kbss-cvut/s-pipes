a5800df9427cc62d4f1a1fbe64303b46978222fb
Miron Grishchenko <grishmirr@gmail.com>
2023-03-07 23:57:41 +0100

    [New] Find triple WIP

--- s-pipes-debug/src/main/java/cz/cvut/spipes/debug/service/ScriptService.java
+++ s-pipes-debug/src/main/java/cz/cvut/spipes/debug/service/ScriptService.java
@@ -1,15 +1,14 @@
 package cz.cvut.spipes.debug.service;
 
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
-import cz.cvut.spipes.Vocabulary;
 import cz.cvut.spipes.debug.model.ModuleExecution;
 import cz.cvut.spipes.debug.persistance.dao.TransformationDao;
+import cz.cvut.spipes.debug.tree.ExecutionTree;
 import cz.cvut.spipes.manager.SPipesScriptManager;
-import cz.cvut.spipes.model.Transformation;
 import cz.cvut.spipes.modules.Module;
 import cz.cvut.spipes.util.ScriptManagerFactory;
 
@@ -20,8 +19,11 @@ public class ScriptService {
 
     private final TransformationDao transformationDao;
 
-    public ScriptService(TransformationDao transformationDao) {
+    private final DebugService debugService;
+
+    public ScriptService(TransformationDao transformationDao, DebugService debugService) {
         this.transformationDao = transformationDao;
+        this.debugService = debugService;
         scriptManager = ScriptManagerFactory.getSingletonSPipesScriptManager();
     }
 
@@ -39,13 +41,63 @@ public class ScriptService {
     //
     //    }
 
+    /*
+        SELECT ?subject ?predicate ?object ?context
+        WHERE {
+        GRAPH ?context {
+            ?subject ?predicate ?object
+            FILTER(REGEX(str(?context), "^http://onto.fel.cvut.cz/ontologies/dataset-descriptor/transformation/1678036284039000.*output$"))
+            FILTER(?subject = <http://onto.fel.cvut.cz/ontologies/s-pipes/hello-world-example-0.1/aaaa-bbbbb>)
+            FILTER(?predicate = <http://onto.fel.cvut.cz/ontologies/s-pipes/hello-world-example-0.1/is-greeted-by-message>)
+            FILTER(?object = "Hello AAAA bbbbb.")
+  }
+}
+
+        SELECT ?s ?p ?o
+        WHERE {
+          GRAPH <http://onto.fel.cvut.cz/ontologies/dataset-descriptor/transformation/1678036284039000/script> {
+            ?s ?p ?o .
+            FILTER (?p = <http://topbraid.org/sparqlmotion#next>)
+          }
+        }
+     */
     public List<ModuleExecution> findTripleOrigin(String executionId, String pattern) {
-        Transformation pipelineExecution = transformationDao.findByUri(Vocabulary.s_c_transformation + "/" + executionId);
-        String scriptName = pipelineExecution.getName();
-        Set<String> global = scriptManager.getGlobalScripts();
-        //        Module module = scriptManager.loadFunction(id);
-        //        recursiveFindTriple(0, )
-        System.out.println(scriptName);
+        List<String> context = transformationDao.findContexts("whatever");
+        List<String> iris = context.stream()
+                .map(i -> i.replace("/output", ""))
+                .collect(Collectors.toList());
+        List<ModuleExecution> moduleExecutions = debugService.getAllModulesForExecution("1678036284039000", null);
+        ExecutionTree executionTree = new ExecutionTree(moduleExecutions);
+        System.out.println(context);
         return null;
     }
 }
+
+//        Repository repository = new HTTPRepository("http://localhost:8081/rdf4j-server", "s-pipes-hello-world");
+//        RepositoryConnection con = repository.getConnection();
+//        Model jenaModel = ModelFactory.createDefaultModel();
+//        IRI context = con.getValueFactory().createIRI("http://onto.fel.cvut.cz/ontologies/dataset-descriptor/transformation/1678036284039000/script");
+//        con.getStatements(null, null, null, context).forEach(statement -> {
+//            Resource subject = ResourceFactory.createResource(statement.getSubject().stringValue());
+//            Property predicate = ResourceFactory.createProperty(statement.getPredicate().stringValue());
+//            RDFNode object = null;
+//
+//            if (statement.getObject() instanceof Literal) {
+//                Literal literal = (Literal) statement.getObject();
+//                object = ResourceFactory.createTypedLiteral(literal.getValue().toString(), literal.getDatatype());
+//            } else {
+//                object = ResourceFactory.createResource(statement.getObject().stringValue());
+//            }
+//
+//            jenaModel.add(subject, predicate, object);
+//        });
+//        con.close();
+//        System.out.println(jenaModel);
+//        Resource resource = jenaModel.getResource("http://onto.fel.cvut.cz/ontologies/dataset-descriptor/transformation/1678036284039000/script");
+//        Resource module = JenaPipelineUtils.getAllFunctionsWithReturnModules(resource.getModel()).get(resource);
+//        System.out.println(module);
+//1 Get Script Rdf4j Model
+//2 Convert it to jena model
+//3 Get List<Module/Resoruce>, so i can have next modules
+//4 Check if output of the modules contain given triple from big depth
+//5
