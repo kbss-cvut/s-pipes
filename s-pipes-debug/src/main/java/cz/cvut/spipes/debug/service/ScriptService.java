db9905543e0aef610698b4211ff5e76888ea6605
Miron Grishchenko <grishmirr@gmail.com>
2023-04-03 22:57:27 +0200

    Refactor + error handling

--- s-pipes-debug/src/main/java/cz/cvut/spipes/debug/service/ScriptService.java
+++ s-pipes-debug/src/main/java/cz/cvut/spipes/debug/service/ScriptService.java
@@ -9,13 +9,12 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import cz.cvut.spipes.debug.exception.NotFoundException;
 import cz.cvut.spipes.debug.mapper.TransformationDtoMapper;
 import cz.cvut.spipes.debug.model.ModuleExecution;
 import cz.cvut.spipes.debug.persistance.dao.TransformationDao;
 import cz.cvut.spipes.debug.tree.ExecutionTree;
-import cz.cvut.spipes.manager.SPipesScriptManager;
 import cz.cvut.spipes.model.Transformation;
-import cz.cvut.spipes.util.ScriptManagerFactory;
 
 
 @Service
@@ -25,24 +24,25 @@ public class ScriptService {
 
     private final TransformationDtoMapper mapper;
 
-    public ScriptService(TransformationDao transformationDao, DebugService debugService, TransformationDtoMapper mapper) {
+    private static final String NOT_FOUND_ERROR = "No matching modules found for pattern %s";
+
+    public ScriptService(TransformationDao transformationDao, TransformationDtoMapper mapper) {
         this.transformationDao = transformationDao;
         this.mapper = mapper;
-        SPipesScriptManager scriptManager = ScriptManagerFactory.getSingletonSPipesScriptManager();
     }
 
     public List<ModuleExecution> findTripleOrigin(String executionId, String graphPattern) {
         Predicate<ModuleExecution> predicate = moduleExecution -> transformationDao.askContainOutput(moduleExecution.getHas_rdf4j_output().getId(), graphPattern);
-        return findFirstModule(executionId, predicate);
+        return findFirstModule(executionId, predicate, graphPattern);
     }
 
     public List<ModuleExecution> findTripleEliminationOrigin(String executionId, String graphPattern) {
         Predicate<ModuleExecution> predicate = moduleExecution -> transformationDao.askContainInputAndNotContainOutput(moduleExecution.getHas_rdf4j_input().getId(),
                 moduleExecution.getHas_rdf4j_output().getId(), graphPattern);
-        return findFirstModule(executionId, predicate);
+        return findFirstModule(executionId, predicate, graphPattern);
     }
 
-    public List<ModuleExecution> findFirstModule(String executionId, Predicate<ModuleExecution> predicate) {
+    public List<ModuleExecution> findFirstModule(String executionId, Predicate<ModuleExecution> predicate, String pattern) {
         String pipelineExecutionIri = getTransformationIriFromId(executionId);
         Transformation transformation = transformationDao.findByUri(pipelineExecutionIri);
 
@@ -53,6 +53,9 @@ public class ScriptService {
             if (predicate.test(moduleExecution)) {
                 modulesWithMatchingPattern.add(moduleExecution);
             }
+        }
+        if (modulesWithMatchingPattern.isEmpty()) {
+            throw new NotFoundException(String.format(NOT_FOUND_ERROR, pattern));
         }
         ExecutionTree executionTree = new ExecutionTree(moduleExecutions);
         return executionTree.findEarliest(modulesWithMatchingPattern);
