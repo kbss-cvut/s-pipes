f1a477f18ccd6791221f8d4c8debe8122949b2b8
Nazarov, Rodion <nazarrod@fel.cvut.cz>
2023-06-28 19:01:56 +0200

    [UPD] Implementation of Rdf4j Update Module

--- s-pipes-modules/module-rdf4j/src/main/java/cz/cvut/spipes/modules/Rdf4jUpdateModule.java
+++ s-pipes-modules/module-rdf4j/src/main/java/cz/cvut/spipes/modules/Rdf4jUpdateModule.java
@@ -1,69 +1,82 @@
 package cz.cvut.spipes.modules;
 
 import cz.cvut.spipes.constants.KBSS_MODULE;
+import cz.cvut.spipes.constants.SML;
 import cz.cvut.spipes.engine.ExecutionContext;
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Literal;
-import org.eclipse.rdf4j.model.util.Values;
-import org.eclipse.rdf4j.query.TupleQuery;
-import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.eclipse.rdf4j.query.*;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
 import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
+import org.topbraid.spin.vocabulary.SP;
+import java.util.List;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
 
 public class Rdf4jUpdateModule extends AbstractModule{
     private static final Logger logger = Logger.getLogger(Rdf4jUpdateModule.class.getName());
     private static final String TYPE_URI = KBSS_MODULE.getURI()+"rdf4j-update";
     private static final String PROPERTY_PREFIX_URI = KBSS_MODULE.getURI()+"rdf4j";
-    Repository queryRepository = new SPARQLRepository("http://localhost:8080/rdf4j-server/repositories/1");
-    Repository updateRepository = new SPARQLRepository("http://localhost:8080/rdf4j-server/repositories/1/statements");
+    private RepositoryConnection updateConnection;
+    static final Property P_RDF4J_SERVER_URL = getParameter("p-rdf4j-server-url");
+    static final Property P_RDF4J_REPOSITORY_NAME = getParameter("p-rdf4j-repository-name");
+    private List<Resource> updateQueries;
 
-    private static void setLogger(){
-        logger.setLevel(Level.ALL);
-        logger.setUseParentHandlers(false);
-        FileHandler fh;
-        boolean dirCreated = new File("./LOGS/").mkdirs();
-        try {
-            fh = new FileHandler("./LOGS/testlogs.txt");
-        }catch (IOException e){
-            return;
+    private static Property getParameter(final String name) {
+        return ResourceFactory.createProperty(PROPERTY_PREFIX_URI + "/" + name);
+    }
+    private String getUpdateType(String updateString){
+        StringBuilder res = new StringBuilder();
+        char[] updateArr = updateString.toCharArray();
+        for (char c : updateArr) {
+            if (c == ' ') break;
+            res.append(c);
         }
-        fh.setFormatter(new SimpleFormatter());
-        logger.addHandler(fh);
+        return res.toString();
     }
 
     @Override
     ExecutionContext executeSelf() {
-        setLogger();
-        logger.info("executeSelf entered");
-        insertQuery();
-        selectQuery("SELECT* WHERE{ ?s ?p ?o }");
+        for (Resource updateQuery : updateQueries) {
+            String text = updateQuery.getProperty(SP.text).getLiteral().getString();
+            String[] queries = text.split("\\}");
+            for (int j = 0; j < queries.length - 1; j++) {
+                queries[j] = (queries[j] + "}").trim();
+                String updateString = queries[j];
+                if( (j < queries.length-2) && (getUpdateType((queries[j + 1]+"}").trim()).equals("WHERE")) ){
+                    updateString += (queries[j+1]+"}").trim();
+                    j++;
+                }
+                makeUpdate(updateString);
+            }
+        }
+        updateConnection.close();
         return this.executionContext;
     }
 
-    void selectQuery(String query){
-        RepositoryConnection connection = queryRepository.getConnection();
-        TupleQuery tupleQuery = connection.prepareTupleQuery(query);
-        TupleQueryResult tupleQueryResult = tupleQuery.evaluate();
-        while(tupleQueryResult.hasNext()){
-            System.out.println(tupleQueryResult.next());
+    void makeUpdate(String updateString){
+        org.eclipse.rdf4j.query.Update prepareUpdate = null;
+        try {
+            prepareUpdate = updateConnection.prepareUpdate(QueryLanguage.SPARQL,updateString);
         }
-    }
-
-    void insertQuery(){
-        RepositoryConnection connection = updateRepository.getConnection();
-        IRI john = Values.iri("http://example.org/people/john");
-        IRI name = Values.iri("http://example.org/ontology/name");
-        Literal johnName = Values.literal("John");
-        connection.add(john,name,johnName);
+        catch (MalformedQueryException e){
+            logger.severe("Malformed Query, query text:\n"+updateString);
+            return;
+        }
+        catch (RepositoryException e){
+            logger.severe("Repository exception\n"+e.getMessage());
+            return;
+        }
+        try {
+            assert prepareUpdate != null;
+            prepareUpdate.execute();
+            logger.info("Update executed");
+        }
+        catch (UpdateExecutionException e){
+            logger.severe("Update execution exception, query text:\n"+updateString+"\n"+e.getMessage());
+        }
     }
 
     @Override
@@ -73,6 +86,16 @@ public class Rdf4jUpdateModule extends AbstractModule{
 
     @Override
     public void loadConfiguration() {
-
+        String rdf4jServerURL = getEffectiveValue(P_RDF4J_SERVER_URL).asLiteral().getString();
+        String rdf4jRepositoryName = getEffectiveValue(P_RDF4J_REPOSITORY_NAME).asLiteral().getString();
+        Repository updateRepository = new SPARQLRepository(rdf4jServerURL + "repositories/" + rdf4jRepositoryName + "/statements");
+        updateQueries = getResourcesByProperty(SML.updateQuery);
+        try {
+            updateConnection = updateRepository.getConnection();
+            logger.info("Connection created for repository "+rdf4jRepositoryName);
+        }
+        catch (RepositoryException e){
+            logger.severe("Repository exception\n"+e.getMessage());
+        }
     }
 }
--- s-pipes-modules/module-rdf4j/src/test/java/cz/cvut/spipes/modules/Rdf4jUpdateModuleTest.java
+++ s-pipes-modules/module-rdf4j/src/test/java/cz/cvut/spipes/modules/Rdf4jUpdateModuleTest.java
@@ -1,6 +1,8 @@
 package cz.cvut.spipes.modules;
 
 import cz.cvut.spipes.engine.ExecutionContext;
+import cz.cvut.spipes.engine.ExecutionContextFactory;
+import org.apache.jena.rdf.model.*;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +14,28 @@ class Rdf4jUpdateModuleTest {
         Rdf4jUpdateModule module = new Rdf4jUpdateModule();
         ExecutionContext newContext = module.executeSelf();
 //        System.out.println(newContext.getDefaultModel().listStatements().toList());
+    }
+
+    @Test
+    @Disabled
+    public void testUpdateEmpty() {
+        final Rdf4jUpdateModule moduleRdf4j = new Rdf4jUpdateModule();
+
+        final Model updateModel = ModelFactory.createDefaultModel();
+        final Property resource = ResourceFactory.createProperty("http://a");
+        updateModel.add(resource, resource, resource);
+
+        final ExecutionContext executionContext = ExecutionContextFactory.createContext(updateModel);
+
+        final Model model = ModelFactory.createDefaultModel();
+        final Resource root = model.createResource();
+        model.add(root, Rdf4jDeployModule.P_RDF4J_SERVER_URL, "http://localhost:8080/rdf4j-server/");
+        model.add(root, Rdf4jDeployModule.P_RDF4J_REPOSITORY_NAME, "1");
+
+        moduleRdf4j.setConfigurationResource(root);
+
+        // TODO: currently running server is needed;
+        moduleRdf4j.setInputContext(executionContext);
+        moduleRdf4j.execute();
     }
 }
