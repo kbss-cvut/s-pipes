1d8d4e849c6038e03e1f8b7d862c38f1f43a248c
Nazarov, Rodion <nazarrod@fel.cvut.cz>
2023-08-02 16:01:07 +0200

    [Fix] Change Log to debug mode

--- s-pipes-modules/module-rdf4j/src/main/java/cz/cvut/spipes/modules/Rdf4jUpdateModule.java
+++ s-pipes-modules/module-rdf4j/src/main/java/cz/cvut/spipes/modules/Rdf4jUpdateModule.java
@@ -109,12 +109,12 @@ public class Rdf4jUpdateModule extends AbstractModule {
             LOG.debug("Connected to {}", rdf4jRepositoryName);
             for(int i = 0;i < iterationCount;i++) {
                 long oldTriplesCount = updateConnection.size();
-                LOG.info("Number of triples before execution: {}",oldTriplesCount);
+                LOG.debug("Number of triples before execution: {}",oldTriplesCount);
                 for (String updateQuery : updateQueries) {
                     makeUpdate(updateQuery, updateConnection);
                 }
                 long newTriplesCount = updateConnection.size();
-                LOG.info("Number of triples after execution: {}",newTriplesCount);
+                LOG.debug("Number of triples after execution: {}",newTriplesCount);
                 if(onlyIfTripleCountChanges && (newTriplesCount == oldTriplesCount) )break;
             }
         } catch (RepositoryException e) {
