836cfb83f2af14d4f099c31cd09ccabaeb492f27
Miroslav Blasko <blcham@gmail.com>
2023-08-24 10:30:03 +0200

    [Fix] Correct bad function name

--- s-pipes-core/src/main/java/cz/cvut/spipes/function/time/Duration.java
+++ s-pipes-core/src/main/java/cz/cvut/spipes/function/time/Duration.java
@@ -18,7 +18,7 @@ import java.util.Calendar;
  */
 public class Duration extends AbstractFunction2 implements ValueFunction {
 
-    private static final String TYPE_IRI = KBSS_TIMEF.getURI() + "add-days";
+    private static final String TYPE_IRI = KBSS_TIMEF.getURI() + "duration-in-ms";
 
     @Override
     public String getTypeURI() {
