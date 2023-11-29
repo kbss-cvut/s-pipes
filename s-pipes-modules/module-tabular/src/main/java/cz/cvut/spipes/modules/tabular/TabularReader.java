24c10e5df011d866da7de18178d157e36cf6bdee
Nazarov, Rodion <nazarrod@fel.cvut.cz>
2023-11-29 22:51:59 +0100

    [Upd] Refactor creating output columns to the TabularReader

--- s-pipes-modules/module-tabular/src/main/java/cz/cvut/spipes/modules/tabular/TabularReader.java
+++ s-pipes-modules/module-tabular/src/main/java/cz/cvut/spipes/modules/tabular/TabularReader.java
@@ -1,8 +1,12 @@
 package cz.cvut.spipes.modules.tabular;
 
+import cz.cvut.spipes.modules.model.Column;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface TabularReader {
     List<String> getHeader() throws IOException;
+    ArrayList<Column> getOutputColumns(List<String>header);
 }
