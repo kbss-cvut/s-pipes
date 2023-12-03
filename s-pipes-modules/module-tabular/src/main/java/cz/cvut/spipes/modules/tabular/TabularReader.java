6f5cfe9b62b3873f42ef3b08b027242f57e69bba
Nazarov, Rodion <nazarrod@fel.cvut.cz>
2023-12-03 16:59:50 +0100

    [Upd] Refactor getting table.rows columns to the TabularReader

--- s-pipes-modules/module-tabular/src/main/java/cz/cvut/spipes/modules/tabular/TabularReader.java
+++ s-pipes-modules/module-tabular/src/main/java/cz/cvut/spipes/modules/tabular/TabularReader.java
@@ -1,12 +1,17 @@
 package cz.cvut.spipes.modules.tabular;
 
+import cz.cvut.spipes.modules.Mode;
 import cz.cvut.spipes.modules.model.Column;
+import cz.cvut.spipes.modules.model.Row;
+import cz.cvut.spipes.modules.model.TableSchema;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public interface TabularReader {
     List<String> getHeader() throws IOException;
     ArrayList<Column> getOutputColumns(List<String>header);
+    Set<Row> getRows(TableSchema tableSchema, String sourceResourceUri, Mode outputMode) throws IOException;
 }
