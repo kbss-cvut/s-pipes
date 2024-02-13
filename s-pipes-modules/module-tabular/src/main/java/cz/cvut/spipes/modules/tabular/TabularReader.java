a742fbb81d07c92b3817a8de49720a624b26c902
Nazarov, Rodion <nazarrod@fel.cvut.cz>
2024-02-13 14:09:37 +0100

    [Upd] Eliminate usage of the TSV convertors.

--- s-pipes-modules/module-tabular/src/main/java/cz/cvut/spipes/modules/tabular/TabularReader.java
+++ s-pipes-modules/module-tabular/src/main/java/cz/cvut/spipes/modules/tabular/TabularReader.java
@@ -1,17 +1,18 @@
 package cz.cvut.spipes.modules.tabular;
 
-import cz.cvut.spipes.modules.Mode;
 import cz.cvut.spipes.modules.model.Column;
-import cz.cvut.spipes.modules.model.Row;
+import cz.cvut.spipes.modules.model.Region;
 import cz.cvut.spipes.modules.model.TableSchema;
 import org.apache.jena.rdf.model.Statement;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Set;
 
 public interface TabularReader {
     List<String> getHeader() throws IOException;
     List<Statement> getRowStatements(List<String>header, List<Column>outputColumns, TableSchema tableSchema) throws IOException;
     int getNumberOfRows();
+    String getTableName();
+    int getTablesCount();
+    List<Region> getMergedRegions();
 }
