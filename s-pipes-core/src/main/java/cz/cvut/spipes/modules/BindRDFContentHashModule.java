ec04a9f2561e8830a532976d3d8e8590a8a5125c
Daniil Palagin <daniilpalaginjob@gmail.com>
2024-08-05 18:26:17 +0200

    [#219] Remove loadConfiguration wherever it possible and add parameter annotation to the attributes. Change loadConfiguration to support assign RDFNode itself.

--- s-pipes-core/src/main/java/cz/cvut/spipes/modules/BindRDFContentHashModule.java
+++ s-pipes-core/src/main/java/cz/cvut/spipes/modules/BindRDFContentHashModule.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 public class BindRDFContentHashModule extends AbstractModule {
 
     private static final Logger LOG = LoggerFactory.getLogger(BindRDFContentHashModule.class);
+    @Parameter(urlPrefix =  SM.uri, name="outputVariable", comment = "")
     String outputVariable;
 
     @Override
