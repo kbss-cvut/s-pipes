ac8bf7c25868e5943740315f5dd916736173c0e2
Daniil Palagin <daniilpalaginjob@gmail.com>
2024-08-18 19:12:08 +0200

    [#219] Refactor ListHandler.

--- s-pipes-core/src/main/java/cz/cvut/spipes/modules/handlers/ListHandler.java
+++ s-pipes-core/src/main/java/cz/cvut/spipes/modules/handlers/ListHandler.java
@@ -2,6 +2,7 @@ package cz.cvut.spipes.modules.handlers;
 
 import cz.cvut.spipes.engine.ExecutionContext;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 
 import java.lang.reflect.ParameterizedType;
@@ -16,48 +17,20 @@ public class ListHandler extends Handler<List<?>> {
 
     private final Map<Class<?>, Function<Object, ?>> handlerMap = new HashMap<>();
 
-
     public ListHandler(Resource resource, ExecutionContext executionContext, Setter<? super List<?>> setter) {
         super(resource, executionContext, setter);
-        registerHandlers();
     }
-
-    private void registerHandlers() {
-        handlerMap.put(Resource.class, this::getResourcesByProperty);
-    }
-
-
-    Function<Object, ?> getHandler() {
-        Type genericType = setter.getField().getGenericType();
-        if (genericType instanceof ParameterizedType parameterizedType) {
-            Type[] typeArguments = parameterizedType.getActualTypeArguments();
-            if (typeArguments.length > 0) {
-                Type typeArgument = typeArguments[0];
-                if (typeArgument instanceof Class<?>) {
-                    return handlerMap.get(typeArgument);
-                }
-            }
-        }
-        return null;
-    }
-
-
 
     @Override
     public void setValueByProperty(Property property) {
-        Function<Object, ?> handler = getHandler();
-        if(handler != null) {
-            List<Resource> list = (List<Resource>) handler.apply(property);
-            setter.addValue(list);
-        }
+        setter.addValue(getRDFNodeListByProperty(property));
     }
 
-    private List<Resource> getResourcesByProperty(Object property) {
+    public List<RDFNode> getRDFNodeListByProperty(Object property) {
         return resource.listProperties((Property) property)
                 .toList().stream()
                 .map(st -> st.getObject().asResource())
                 .collect(Collectors.toList());
     }
-
 
 }
--- s-pipes-core/src/test/java/cz/cvut/spipes/modules/handlers/BooleanHandlerTest.java
+++ s-pipes-core/src/test/java/cz/cvut/spipes/modules/handlers/BooleanHandlerTest.java
@@ -1,63 +1,73 @@
 package cz.cvut.spipes.modules.handlers;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import cz.cvut.spipes.engine.ExecutionContext;
-import org.apache.jena.rdf.model.*;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Property;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
 
 public class BooleanHandlerTest {
 
     private BooleanHandler booleanHandler;
-    private Resource mockResource;
-    private ExecutionContext mockExecutionContext;
-    private Setter<Boolean> mockSetter;
-    private Property mockProperty;
-    private Model model;
+    private Resource resource;
+    private ExecutionContext executionContext;
+    private Setter<Boolean> setter;
+    private RDFNode rdfNode;
 
     @BeforeEach
     public void setUp() {
-        mockResource = mock(Resource.class);
-        mockExecutionContext = mock(ExecutionContext.class);
-        mockSetter = mock(Setter.class);
-        mockProperty = mock(Property.class);
-        model = ModelFactory.createDefaultModel();
-
-        booleanHandler = new BooleanHandler(mockResource, mockExecutionContext, mockSetter);
+        resource = mock(Resource.class);
+        executionContext = mock(ExecutionContext.class);
+        setter = mock(Setter.class);
+        booleanHandler = new BooleanHandler(resource, executionContext, setter);
     }
 
     @Test
-    public void testSetValueByPropertyWithBooleanLiteral() {
-        RDFNode booleanLiteralNode = model.createLiteral(String.valueOf(true));
-        Statement mockStatement = mock(Statement.class);
+    public void testGetJavaNativeValueTrue() {
+        Literal literal = mock(Literal.class);
+        when(literal.getBoolean()).thenReturn(true);
+        rdfNode = mock(RDFNode.class);
+        when(rdfNode.asLiteral()).thenReturn(literal);
 
-        when(mockResource.getProperty(mockProperty)).thenReturn(mockStatement);
-        when(mockStatement.getObject()).thenReturn(booleanLiteralNode);
-
-        booleanHandler.setValueByProperty(mockProperty);
-
-        verify(mockSetter).addValue(true);
+        Boolean result = booleanHandler.getJavaNativeValue(rdfNode);
+        assertTrue(result);
     }
 
     @Test
-    public void testSetValueByPropertyWithNonLiteralNode() {
-        RDFNode nonLiteralNode = model.createResource();
-        Statement mockStatement = mock(Statement.class);
+    public void testGetJavaNativeValueFalse() {
+        Literal literal = mock(Literal.class);
+        when(literal.getBoolean()).thenReturn(false);
+        rdfNode = mock(RDFNode.class);
+        when(rdfNode.asLiteral()).thenReturn(literal);
 
-        when(mockResource.getProperty(mockProperty)).thenReturn(mockStatement);
-        when(mockStatement.getObject()).thenReturn(nonLiteralNode);
-
-        booleanHandler.setValueByProperty(mockProperty);
-
-        verify(mockSetter, never()).addValue(anyBoolean());
+        Boolean result = booleanHandler.getJavaNativeValue(rdfNode);
+        assertFalse(result);
     }
 
     @Test
-    public void testSetValueByPropertyWithNullNode() {
-        when(mockResource.getProperty(mockProperty)).thenReturn(null);
+    public void testGetJavaNativeValueInvalidType() {
+        Literal literal = mock(Literal.class);
+        when(literal.getBoolean()).thenThrow(new ClassCastException("Not a boolean"));
+        rdfNode = mock(RDFNode.class);
+        when(rdfNode.asLiteral()).thenReturn(literal);
 
-        booleanHandler.setValueByProperty(mockProperty);
-
-        verify(mockSetter, never()).addValue(anyBoolean());
+        assertThrows(ClassCastException.class, () -> booleanHandler.getJavaNativeValue(rdfNode));
     }
+
+    @Test
+    public void testSetValueByPropertyNullNode() {
+        Property property = mock(Property.class);
+        when(booleanHandler.getEffectiveValue(property)).thenReturn(null);
+
+        booleanHandler.setValueByProperty(property);
+
+        verify(setter, never()).addValue(anyBoolean());
+    }
+
 }
