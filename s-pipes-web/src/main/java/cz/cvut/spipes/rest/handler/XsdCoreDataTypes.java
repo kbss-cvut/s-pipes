e9965e4abc369db0d95853cb53812fd8149b07b3
Daniil Palagin <daniilpalaginjob@gmail.com>
2024-09-20 11:15:37 +0200

    [#279] Implement CoreDataTypes resolving

---
+++ s-pipes-web/src/main/java/cz/cvut/spipes/rest/handler/XsdCoreDataTypes.java
@@ -0,0 +1,23 @@
+package cz.cvut.spipes.rest.handler;
+
+import java.util.List;
+
+public class XsdCoreDataTypes {
+
+    public static final String XSD_NAMESPACE = "http://www.w3.org/2001/XMLSchema#";
+
+    public static final String XSD_STRING = XSD_NAMESPACE + "string";
+    public static final String XSD_INTEGER = XSD_NAMESPACE + "integer";
+    public static final String XSD_BOOLEAN = XSD_NAMESPACE + "boolean";
+    public static final String XSD_DECIMAL = XSD_NAMESPACE + "decimal";
+    public static final String XSD_DOUBLE = XSD_NAMESPACE + "double";
+
+    public static final List<String> CORE_TYPES = List.of(XSD_STRING, XSD_INTEGER, XSD_BOOLEAN, XSD_DECIMAL, XSD_DOUBLE);
+
+    public static boolean isCorePrimitiveType(String type) {
+        if(type == null) {
+            return false;
+        }
+        return CORE_TYPES.contains(type);
+    }
+}
