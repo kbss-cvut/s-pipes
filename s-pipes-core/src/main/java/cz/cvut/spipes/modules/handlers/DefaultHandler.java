5822f35ab54a2708d50d42067d663b19a6a1606f
Miroslav Blasko <blcham@gmail.com>
2024-12-17 11:48:07 +0100

    [#288] Initial implementation of handler

---
+++ s-pipes-core/src/main/java/cz/cvut/spipes/modules/handlers/DefaultHandler.java
@@ -0,0 +1,26 @@
+package cz.cvut.spipes.modules.handlers;
+
+import cz.cvut.spipes.engine.ExecutionContext;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
+/**
+ * The {@link DefaultHandler} class is a default implementation of the {@link Handler} interface.
+ * It uses the {@link HandlerRegistry} to retrieve the appropriate handler for the given field type.
+ **/
+public class DefaultHandler extends Handler {
+
+    private Handler<?> typeHandler;
+
+    public DefaultHandler(Resource resource, ExecutionContext executionContext, Setter setter) {
+        super(resource, executionContext, setter);
+
+        HandlerRegistry handlerRegistry = HandlerRegistry.getInstance();
+        Handler<?> typeHandler = handlerRegistry.getHandler(setter.getField().getType(), resource, executionContext, setter);
+    }
+
+    @Override
+    public void setValueByProperty(Property property) {
+        typeHandler.setValueByProperty(property);
+    }
+}
