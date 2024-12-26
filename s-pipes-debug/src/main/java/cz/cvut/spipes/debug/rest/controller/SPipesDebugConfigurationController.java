2e2dccfc443e72a27bd84d0493ec61dd8f261c87
Daniil Palagin <daniilpalaginjob@gmail.com>
2024-12-26 19:25:31 +0100

    [New] Migrate from SpringFox to openApi

--- s-pipes-debug/src/main/java/cz/cvut/spipes/debug/rest/controller/SPipesDebugConfigurationController.java
+++ s-pipes-debug/src/main/java/cz/cvut/spipes/debug/rest/controller/SPipesDebugConfigurationController.java
@@ -1,11 +1,11 @@
 package cz.cvut.spipes.debug.rest.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cz.cvut.spipes.debug.service.ConfigurationService;
-import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class SPipesDebugConfigurationController {
@@ -16,7 +16,7 @@ public class SPipesDebugConfigurationController {
         this.configurationService = configurationService;
     }
 
-    @ApiOperation(value = "Change rdf4j repository for SPipes debug")
+    @Operation(summary = "Change rdf4j repository for SPipes debug")
     @PutMapping("/repository/{repositoryName}")
     public void changeRepository(@PathVariable String repositoryName) {
         configurationService.changeRepository(repositoryName);
