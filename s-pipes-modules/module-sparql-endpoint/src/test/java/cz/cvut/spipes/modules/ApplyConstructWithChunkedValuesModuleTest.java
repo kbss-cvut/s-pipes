efe78b05de96380d717196ece821debe7866fda6
Evgenii Grigorev <grigoevg@fel.cvut.cz>
2025-01-27 11:27:34 +0100

    [#294] Removed duplicate AbstractModuleTestHelper.java

--- s-pipes-modules/module-sparql-endpoint/src/test/java/cz/cvut/spipes/modules/ApplyConstructWithChunkedValuesModuleTest.java
+++ s-pipes-modules/module-sparql-endpoint/src/test/java/cz/cvut/spipes/modules/ApplyConstructWithChunkedValuesModuleTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 
 
-public class ApplyConstructWithChunkedValuesModuleTest extends AbstractModuleTestHelper {
+public class ApplyConstructWithChunkedValuesModuleTest extends AbstractSparqlEndpointModuleTestHelper {
 
     @BeforeEach
     public void setUp() {
