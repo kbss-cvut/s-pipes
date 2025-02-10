288dacc2b8d57d42f63ad7a76295296fae046fd8
Miroslav Blasko <blcham@gmail.com>
2025-02-10 09:41:56 +0100

    Reformat

--- s-pipes-debug/src/main/java/cz/cvut/spipes/debug/persistance/dao/InputBindingDao.java
+++ s-pipes-debug/src/main/java/cz/cvut/spipes/debug/persistance/dao/InputBindingDao.java
@@ -1,11 +1,10 @@
 package cz.cvut.spipes.debug.persistance.dao;
 
-import static cz.cvut.spipes.engine.VariablesBinding.HAS_BOUND_VARIABLE;
-
-import org.springframework.stereotype.Repository;
-
 import cz.cvut.kbss.jopa.model.EntityManager;
 import cz.cvut.spipes.model.Thing;
+import org.springframework.stereotype.Repository;
+
+import static cz.cvut.spipes.engine.VariablesBinding.HAS_BOUND_VARIABLE;
 
 @Repository
 public class InputBindingDao extends AbstractDao<Thing> {
@@ -14,11 +13,12 @@ public class InputBindingDao extends AbstractDao<Thing> {
     }
 
     public boolean askHasBoundVariable(String binding, String variableName) {
-        String query = String.format("ASK { "
-                + "  GRAPH <%s>{ "
-                + "    ?s <%s> \"%s\""
-                + "  }"
-                + "}", binding, HAS_BOUND_VARIABLE, variableName);
+        String query = String.format("""
+                ASK {
+                  GRAPH <%s>{
+                    ?s <%s> "%s"
+                  }
+                }""", binding, HAS_BOUND_VARIABLE, variableName);
         try {
             return (Boolean) em.createNativeQuery(query).getSingleResult();
         } catch (Exception e) {
