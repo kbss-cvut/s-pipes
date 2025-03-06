bb422eaed864506abf759f9a74fe128124d31f84
Miroslav Blasko <blcham@gmail.com>
2025-03-06 17:32:31 +0100

    Migrate parts from spring-boot to spring framework

--- s-pipes-debug/src/main/java/cz/cvut/spipes/debug/config/SwaggerConfig.java
+++ s-pipes-debug/src/main/java/cz/cvut/spipes/debug/config/SwaggerConfig.java
@@ -1,17 +1,34 @@
 package cz.cvut.spipes.debug.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "SPipes Debug Open API",
-                description = "SPipes Debug Open API documentation",
-                version = "1.0"
-        )
-)
+@EnableSwagger2
 public class SwaggerConfig {
 
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.regex("/api/.*"))
+            .build()
+            .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("SPipes Debug Open API")
+            .description("SPipes Debug Open API documentation")
+            .version("1.0")
+            .build();
+    }
 }
