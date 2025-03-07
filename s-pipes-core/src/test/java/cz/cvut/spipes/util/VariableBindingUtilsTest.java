365c5bad45097e28a9624a559160d68cd0f249ab
Evgenii Grigorev <grigoevg@fel.cvut.cz>
2025-03-07 13:29:06 +0100

    [#227] Add test for loading data from HTTP

--- s-pipes-core/src/test/java/cz/cvut/spipes/util/VariableBindingUtilsTest.java
+++ s-pipes-core/src/test/java/cz/cvut/spipes/util/VariableBindingUtilsTest.java
@@ -1,53 +1,82 @@
 package cz.cvut.spipes.util;
 import cz.cvut.spipes.engine.VariablesBinding;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 import static cz.cvut.spipes.util.VariableBindingUtils.extendBindingFromURL;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.reflections.Reflections.log;
 
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
 class VariableBindingUtilsTest {
+
+    private String testData = """
+            <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036/personId>
+                    <http://onto.fel.cvut.cz/ontologies/s-pipes/has_bound_value>
+                            "robert-plant" ;
+                    <http://onto.fel.cvut.cz/ontologies/s-pipes/has_bound_variable>
+                            "personId" .
+                            
+            <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036/personName>
+                    <http://onto.fel.cvut.cz/ontologies/s-pipes/has_bound_value>
+                            "Robert Plant" ;
+                    <http://onto.fel.cvut.cz/ontologies/s-pipes/has_bound_variable>
+                            "personName" .
+                            
+            <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036>
+                    a       <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution> ;
+                    <http://onto.fel.cvut.cz/ontologies/s-pipes/has_binding>
+                            <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036/personId>, 
+                            <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036/personName>.                                             
+            """;
+
+    private HttpServer server;
+
+    @BeforeEach
+    void setup() throws IOException {
+        server = HttpServer.create(new InetSocketAddress(0), 0);
+        server.createContext("/test.ttl", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                exchange.sendResponseHeaders(200, testData.getBytes().length);
+                exchange.getResponseBody().write(testData.getBytes());
+                exchange.close();
+            }
+        });
+        server.setExecutor(Executors.newCachedThreadPool());
+        server.start();
+    }
+
+    @AfterEach
+    void teardown() {
+        server.stop(0);
+    }
 
     @Test
     void extendBindingFromURLLoadsLocalDataCorrectly(@TempDir File tempDir) throws Exception {
         File tempFile = new File(tempDir, "test.ttl");
 
         try (FileOutputStream fos = new FileOutputStream(tempFile)) {
-            String testData = """
-                    <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036/personId>
-                            <http://onto.fel.cvut.cz/ontologies/s-pipes/has_bound_value>
-                                    "robert-plant" ;
-                            <http://onto.fel.cvut.cz/ontologies/s-pipes/has_bound_variable>
-                                    "personId" .
-                                    
-                    <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036/personName>
-                            <http://onto.fel.cvut.cz/ontologies/s-pipes/has_bound_value>
-                                    "Robert Plant" ;
-                            <http://onto.fel.cvut.cz/ontologies/s-pipes/has_bound_variable>
-                                    "personName" .
-                                    
-                    <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036>
-                            a       <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution> ;
-                            <http://onto.fel.cvut.cz/ontologies/s-pipes/has_binding>
-                                    <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036/personId>, 
-                                    <http://onto.fel.cvut.cz/ontologies/s-pipes/query_solution_1740574722036/personName>.                                             
-                    """;
             fos.write(testData.getBytes());
         }
 
         // Emulating saving of the path like SaveModelToTemporaryFile does
         // Reading URL from parameters like ServiceParametersHelper does
-        URL fileURL = new URL("file://" + tempFile.getAbsolutePath());
-
-        // Working way you read URL
-        //URL fileURL = tempFile.toURI().toURL();
+        String filePath = tempFile.toURI().toURL().toString();
+        URL fileURL = new URL(filePath);
 
         VariablesBinding inputVariablesBinding = new VariablesBinding();
         extendBindingFromURL(inputVariablesBinding, fileURL);
@@ -55,5 +84,17 @@ class VariableBindingUtilsTest {
         assertEquals("robert-plant", inputVariablesBinding.getNode("personId").toString());
         assertEquals("Robert Plant", inputVariablesBinding.getNode("personName").toString());
     }
-}
 
+    @Test
+    void extendBindingFromURLLoadsDataFromHttpUrlCorrectly() throws Exception {
+        URL url = new URL("http://localhost:" + server.getAddress().getPort() + "/test.ttl");
+
+        VariablesBinding inputVariablesBinding = new VariablesBinding();
+        extendBindingFromURL(inputVariablesBinding, url);
+
+        assertEquals("robert-plant", inputVariablesBinding.getNode("personId").toString());
+        assertEquals("Robert Plant", inputVariablesBinding.getNode("personName").toString());
+
+    }
+
+}
