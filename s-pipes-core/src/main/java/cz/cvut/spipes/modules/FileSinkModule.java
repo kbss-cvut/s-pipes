b75c7620aa2c0d084be8f4d217a776eafd18a434
Bogdan Kostov <kostobog@fel.cvut.cz>
2025-06-11 18:39:00 +0200

    [Fix #382] Add FileSinkModule allowing to store files in ws directory. - extending Module api to allow for modules to get the rootFile of the current execution.

---
+++ s-pipes-core/src/main/java/cz/cvut/spipes/modules/FileSinkModule.java
@@ -0,0 +1,108 @@
+package cz.cvut.spipes.modules;
+
+import cz.cvut.spipes.constants.KBSS_MODULE;
+import cz.cvut.spipes.constants.SML;
+import cz.cvut.spipes.engine.ExecutionContext;
+import cz.cvut.spipes.manager.SPipesScriptManager;
+import cz.cvut.spipes.manager.factory.ScriptManagerFactory;
+import cz.cvut.spipes.modules.annotations.SPipesModule;
+import cz.cvut.spipes.util.QueryUtils;
+import org.apache.jena.query.*;
+import org.eclipse.rdf4j.common.io.IOUtil;
+import cz.cvut.spipes.spin.model.Select;
+
+import java.io.File;
+import java.io.IOException;
+
+@SPipesModule(label = "Persist model to files in workspace", comment = """
+        """)
+public class FileSinkModule extends AnnotatedAbstractModule {
+
+    static final String FILE_NAME_VAR = "fileName"; // 1
+    static final String FILE_CONTENT_VAR = "content"; // 2
+
+    
+    static final String TYPE_URI = KBSS_MODULE.uri + "file-sink";
+    static final String TYPE_PREFIX = TYPE_URI + "/";
+    static final String FILE_EXTENSION_PARAM = TYPE_PREFIX + "file-extension";
+    static final String OUTPUT_DIRECTORY_PARAM = TYPE_PREFIX + "output-directory";
+    
+    @Parameter(iri = OUTPUT_DIRECTORY_PARAM)
+    private String outputDirectory;
+
+    @Parameter(iri = SML.selectQuery,
+            comment = "The select query that will be used to extract file name and file content. ")
+    private Select selectQuery;
+
+
+    @Override
+    public ExecutionContext executeSelf() {
+
+        File root = computeOutputDirectory();
+                
+        Query query = QueryUtils.createQuery(selectQuery);
+        QuerySolution inputBindings = executionContext.getVariablesBinding().asQuerySolution();
+        try(QueryExecution execution = QueryExecutionFactory.create(query, executionContext.getDefaultModel(), inputBindings)) {
+            ResultSet rs = execution.execSelect();
+            while (rs.hasNext()) {
+                QuerySolution qs = rs.next();
+                String fileName = qs.get(FILE_NAME_VAR).toString();
+                String content = qs.get(FILE_CONTENT_VAR).toString();
+                File f = new File(root, fileName);
+                try {
+                    IOUtil.writeString(content, f);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return executionContext;
+    }
+
+    protected File computeOutputDirectory(){
+        File rootFile = getRootScriptFile();
+
+        if(outputDirectory == null)
+            return rootFile.getParentFile();
+
+        File outputDirectoryFile = new File(outputDirectory);
+        return outputDirectoryFile.isAbsolute() 
+                ? outputDirectoryFile
+                : new File(rootFile.getParentFile(), outputDirectory);
+    }
+    
+//    protected String getContextDirectory(){
+//        SPipesScriptManager scriptManager = ScriptManagerFactory.getSingletonSPipesScriptManager();
+//
+//        executionContext.getVariablesBinding().getNode("_pId");
+////        Binding b = BindingFactory.binding(Var.alloc("currentModule"), resource.asNode());
+////        QueryExecutionFactory.create("""
+////                ?currentModule sm:next :transform-data_Return
+////                ?function sm:returnModule
+////                """, resource.getModel()).setInitialBinding(b);
+//    }
+
+
+    public String getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    public void setOutputDirectory(String outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    public Select getSelectQuery() {
+        return selectQuery;
+    }
+
+    public void setSelectQuery(Select selectQuery) {
+        this.selectQuery = selectQuery;
+    }
+
+    @Override
+    public String getTypeURI() {
+        return TYPE_URI;
+    }
+    
+}
