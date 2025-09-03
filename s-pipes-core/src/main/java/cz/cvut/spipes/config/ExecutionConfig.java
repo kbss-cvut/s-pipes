9d6657e51ec4ee345bb9df6e43a073f3843137e7
Evgenii Grigorev <grigoevg@fel.cvut.cz>
2025-09-03 10:10:11 +0200

    [#324] Fix the typo in the class name

--- s-pipes-core/src/main/java/cz/cvut/spipes/config/ExecutionConfig.java
+++ s-pipes-core/src/main/java/cz/cvut/spipes/config/ExecutionConfig.java
@@ -1,43 +1,43 @@
 package cz.cvut.spipes.config;
 
-import cz.cvut.spipes.util.CoreConfigProperies;
+import cz.cvut.spipes.util.CoreConfigProperties;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ExecutionConfig {
 
     public static Path getTempDirectoryPath() {
-        return Paths.get(CoreConfigProperies.get("execution.tempDirectoryPath", System.getProperty("java.io.tmpdir")));
+        return Paths.get(CoreConfigProperties.get("execution.tempDirectoryPath", System.getProperty("java.io.tmpdir")));
     }
 
     public static boolean isExitOnError() {
-        return Boolean.parseBoolean(CoreConfigProperies.get(
+        return Boolean.parseBoolean(CoreConfigProperties.get(
                 "execution.exitOnError",
                 "false"));
     }
 
     public static boolean isCheckValidationConstrains() {
-        return Boolean.parseBoolean(CoreConfigProperies.get(
+        return Boolean.parseBoolean(CoreConfigProperties.get(
                 "execution.validation.checkConstraints",
                 "true"));
     }
 
     public static int getEvidenceNumber() {
-        return Integer.parseInt(CoreConfigProperies.get("execution.validation.maxNumberOfConstraintFailureEvidences", "3"));
+        return Integer.parseInt(CoreConfigProperties.get("execution.validation.maxNumberOfConstraintFailureEvidences", "3"));
     }
 
     public static String getConfigUrl() {
-        return CoreConfigProperies.get("execution.configUrl", "config.ttl");
+        return CoreConfigProperties.get("execution.configUrl", "config.ttl");
     }
 
     public static Environment getEnvironment() {
-        return  Environment.valueOf(CoreConfigProperies.get(
+        return  Environment.valueOf(CoreConfigProperties.get(
             "execution.environment",
             Environment.production.toString())
         );
     }
 
     public static String getDevelopmentServiceUrl() {
-        return CoreConfigProperies.get("execution.developmentServiceUrl", "http://localhost:8080/s-pipes/");
+        return CoreConfigProperties.get("execution.developmentServiceUrl", "http://localhost:8080/s-pipes/");
     }
 }
