94c6ae344b3bfea15816caf7aed49e3698058249
Miroslav Blasko <blcham@gmail.com>
2025-11-19 17:34:06 +0100

    Fix JsonLd mapping of SPipes Debug REST API

--- s-pipes-debug/src/main/java/cz/cvut/spipes/debug/dto/PipelineExecutionDto.java
+++ s-pipes-debug/src/main/java/cz/cvut/spipes/debug/dto/PipelineExecutionDto.java
@@ -1,15 +1,11 @@
 package cz.cvut.spipes.debug.dto;
 
+import cz.cvut.kbss.jopa.model.annotations.*;
+import cz.cvut.spipes.Vocabulary;
+
 import java.net.URI;
 import java.util.Date;
 import java.util.List;
-
-import cz.cvut.kbss.jopa.model.annotations.OWLClass;
-import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
-import cz.cvut.kbss.jopa.model.annotations.OWLObjectProperty;
-import cz.cvut.kbss.jopa.model.annotations.ParticipationConstraint;
-import cz.cvut.kbss.jopa.model.annotations.ParticipationConstraints;
-import cz.cvut.spipes.Vocabulary;
 
 @OWLClass(iri = Vocabulary.s_c_pipeline_execution)
 public class PipelineExecutionDto extends ExecutionThing {
@@ -23,11 +19,21 @@ public class PipelineExecutionDto extends ExecutionThing {
     })
     private List<ModuleExecutionDto> has_module_executions;
 
+    @OWLDataProperty(iri = Vocabulary.s_p_has_pipeline_execution_finish_date)
+    private Date has_pipeline_execution_finish_date;
+
+    @OWLObjectProperty(iri = Vocabulary.s_p_has_script)
+    private URI has_script;
+
+    @OWLObjectProperty(iri = Vocabulary.s_p_has_function)
+    private URI has_function;
+
+    @OWLDataProperty(iri = Vocabulary.s_p_has_script_path)
+    private String has_script_path;
+
     public Date getHas_pipepline_execution_date() {
         return has_pipepline_execution_date;
     }
-
-    private Date has_pipeline_execution_finish_date;
 
     public Date getHas_pipeline_execution_finish_date() {
         return has_pipeline_execution_finish_date;
@@ -40,9 +46,6 @@ public class PipelineExecutionDto extends ExecutionThing {
     public void setHas_pipepline_execution_date(Date has_pipepline_execution_date) {
         this.has_pipepline_execution_date = has_pipepline_execution_date;
     }
-
-    private URI has_script;
-
     public URI getHas_script() {
         return has_script;
     }
@@ -51,8 +54,6 @@ public class PipelineExecutionDto extends ExecutionThing {
         this.has_script = has_script;
     }
 
-    private URI has_function;
-
     public URI getHas_function() {
         return has_function;
     }
@@ -60,8 +61,6 @@ public class PipelineExecutionDto extends ExecutionThing {
     public void setHas_function(URI has_function) {
         this.has_function = has_function;
     }
-
-    private String has_script_path;
 
     public String getHas_script_path() {
         return has_script_path;
@@ -78,6 +77,5 @@ public class PipelineExecutionDto extends ExecutionThing {
     public void setHas_module_executions(List<ModuleExecutionDto> has_module_executions) {
         this.has_module_executions = has_module_executions;
     }
-
 }
 
